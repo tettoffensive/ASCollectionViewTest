04a3ffa3c2fc58a9e12eee8c9c40623f147ce01d
stuart.tett@gmail.com <stuart@binarythumb.com>
2015-09-01 15:30:25 -0700

    add a title property to the channel

--- Channels/Channels/ChannelModel.h
+++ Channels/Channels/ChannelModel.h
@@ -7,9 +7,11 @@
 //
 
 #import <Mantle/Mantle.h>
+@class VideoModel;
 
 @interface ChannelModel : MTLModel
 
-@property (nonatomic, copy, readonly) NSArray* videos;
+@property (nonatomic, copy, readonly) NSString *title;
+@property (nonatomic, copy, readonly) NSArray<VideoModel *> *videos;
 
 @end
