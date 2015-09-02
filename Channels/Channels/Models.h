3e6d011d05f9c30e8b29c1130edd57090185366b
Ryan <ryannelwan@gmail.com>
2015-09-02 16:54:05 -0700

    Convenient way of importing all models

---
+++ Channels/Channels/Models.h
@@ -0,0 +1,11 @@
+//
+//  Models.h
+//  Channels
+//
+//  Created by Ryan Nelwan on 9/2/15.
+//  Copyright © 2015 Complex Polygon. All rights reserved.
+//
+
+#import "ChannelModel.h"
+#import "PostModel.h"
+#import "UserModel.h"
