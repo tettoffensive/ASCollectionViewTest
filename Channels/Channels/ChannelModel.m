2a188e25ca1088d9a3af0d86985802e76c64831a
Ryan <ryannelwan@gmail.com>
2015-09-03 11:56:38 -0700

    Fetch all posts for a channel

--- Channels/Channels/ChannelModel.m
+++ Channels/Channels/ChannelModel.m
@@ -7,6 +7,7 @@
 //
 
 #import "ChannelModel.h"
+#import "PostModel.h"
 #import "ChannelsNetworking.h"
 
 @implementation ChannelModel
@@ -16,16 +17,30 @@
     return @{@"channelID": @"id", @"title": @"title"};
 }
 
+- (void)setChannelID:(NSInteger)channelID
+{
+    _channelID = channelID;
+}
+
 #pragma ------------------------------------------------------------------------------------------------------
 #pragma mark - Network Calls
 #pragma ------------------------------------------------------------------------------------------------------
 
 // Fetches all public channels
 
-+ (void)fetchChannelsWithSuccess:(void(^)(NSArray<ChannelModel *> *channels))success andFailure:(void(^)(NSError *error))failure;
++ (void)fetchChannelsWithSuccess:(void(^)(NSArray<ChannelModel *> *channels))success andFailure:(void(^)(NSError *error))failure
 {
     [[ChannelsNetworking sharedInstance] channelsWithSuccess:^(NSArray<ChannelModel *> *channels) {
         if (success) success(channels);
+    } andFailure:^(NSError *error) {
+        if (failure) failure(error);
+    }];
+}
+
+- (void)fetchPostsWithSuccess:(void(^)(NSArray<PostModel *> *posts))success andFailure:(void(^)(NSError *error))failure
+{
+    [[ChannelsNetworking sharedInstance] postsForChannelID:self.channelID withSuccess:^(NSArray<PostModel *> *posts) {
+        if (success) success(posts);
     } andFailure:^(NSError *error) {
         if (failure) failure(error);
     }];
