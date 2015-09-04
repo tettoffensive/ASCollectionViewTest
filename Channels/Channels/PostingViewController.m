6a94937cef9aa5ef66dfd798bdf7927f94a05787
Dana Shakiba <dana0550@me.com>
2015-09-03 17:55:15 -0700

    Fixed dismiss post view controller after upload
    Added delay when dismiss after upload

--- Channels/Channels/PostingViewController.m
+++ Channels/Channels/PostingViewController.m
@@ -235,7 +235,10 @@ static const NSString *kPBJVisionVideoThumbnailKey              = @"PBJVisionVid
     
     if (buttonIndex == alertView.firstOtherButtonIndex) {
         [self uploadVideo:_currentVideo];
-        [self dismissPostingViewController];
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [self dismissPostingViewController];
+        });
     }
 }
 
