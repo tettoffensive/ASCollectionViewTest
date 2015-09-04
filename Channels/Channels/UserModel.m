094a74f254632ca6c1f8747edebc3e602fc846bf
Ryan <ryannelwan@gmail.com>
2015-09-04 14:38:49 -0700

    Current user session

--- Channels/Channels/UserModel.m
+++ Channels/Channels/UserModel.m
@@ -7,17 +7,25 @@
 //
 
 #import "UserModel.h"
+#import "ChannelsNetworking.h"
 
 @implementation UserModel
 
+static UserModel *__currentUser = nil;
+
 + (instancetype)currentUser
 {
-    static UserModel *sharedInstance = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        sharedInstance = [[self alloc] init];
-    });
-    return sharedInstance;
+    if (!__currentUser) __currentUser = [[UserModel alloc] initForCurrentUser];
+    return __currentUser;
+}
+
+- (instancetype)initForCurrentUser
+{
+    if (self == [super init]) {
+        self->_userID = [[UserModel getPropertyForKey:@"userModelUserID"] integerValue];
+        self->_username = [UserModel getPropertyForKey:@"userModelUsername"];
+    }
+    return self;
 }
 
 #pragma ------------------------------------------------------------------------------------------------------
@@ -26,26 +34,68 @@
 
 + (NSDictionary *)JSONKeyPathsByPropertyKey
 {
-    return @{@"userID": @"user_id"};
+    return @{@"userID": @"id", @"username":@"username"};
 }
 
 #pragma ------------------------------------------------------------------------------------------------------
 #pragma mark - Api - Current User (only call on these set of methods for current user session)
 #pragma ------------------------------------------------------------------------------------------------------
 
-- (void)login
+
++ (void)registerWithUsername:(NSString *)username password:(NSString *)password andEmail:(NSString *)email success:(void(^)(UserModel *userModel))success andFailure:(void(^)(NSError *error))failure
 {
     
 }
 
-- (void)isLoggedIn
++ (void)loginWithUsername:(NSString *)username andPassword:(NSString *)password success:(void(^)(UserModel *userModel))success andFailure:(void(^)(NSError *error))failure
 {
+    [[ChannelsNetworking sharedInstance] userLoginWithUsername:username andPassword:password success:^(NSDictionary *responseData){
+        
+        NSString *accessToken = [responseData objectForKey:@"access_token"];
+        [UserModel setProperty:accessToken forKey:@"userModelAccessToken"];
+        
+        UserModel *userModel = [UserModel modelWithDictionary:[responseData objectForKey:@"user"]];
+        [UserModel setProperty:@(userModel.userID) forKey:@"userModelUserID"];
+        [UserModel setProperty:userModel.username forKey:@"userModelUsername"];
+        
+        if (success) {
+            success([UserModel currentUser]);
+        }
+        
+    } andFailure:^(NSError *error) {
+        
+    }];
+}
+
+- (void)logout
+{
+    [UserModel setProperty:@"" forKey:@"userModelAccessToken"];
+    [UserModel setProperty:@(NO) forKey:@"userModelIsLoggedIn"];
     
+    __currentUser = nil;
+}
+
++ (BOOL)isLoggedIn
+{
+    return [[UserModel getPropertyForKey:@"userModelIsLoggedIn"] boolValue];
+}
+
+- (void)loginWithAccessToken:(NSString *)accessToken
+{
+    [UserModel setProperty:accessToken forKey:@"userModelAccessToken"];
+    [UserModel setProperty:@(YES) forKey:@"userModelIsLoggedIn"];
 }
 
 - (NSString *)accessToken
 {
-    return nil;
+    return [UserModel getPropertyForKey:@"userModelAccessToken"];
+}
+
+- (void)fetchCurrentUserInfo
+{
+    if (![UserModel isLoggedIn]) {
+        return;
+    }
 }
 
 #pragma ------------------------------------------------------------------------------------------------------
