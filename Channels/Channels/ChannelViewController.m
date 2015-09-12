76174762f4d1bf5b05ae77232bdf53fdf80146cd
stuart.tett@gmail.com <stuart@binarythumb.com>
2015-09-11 17:14:54 -0700

    dismiss to go back to channel list

--- Channels/Channels/ChannelViewController.m
+++ Channels/Channels/ChannelViewController.m
@@ -16,6 +16,9 @@
 @import KVOController;
 
 @interface ChannelViewController ()<ChannelVideoPlayerControllerDelegate,ChannelVideoPlayerControllerDataSource>
+{
+    UIButton *_closeButton;
+}
 /*!
  *  Player responsible for playing the current channel's stream
  */
@@ -45,6 +48,17 @@
     
     [self.view addSubview:self.postButton];
     [self.view addSubview:self.postTrackerLabel];
+    
+    // Close Button
+    _closeButton = [[UIButton alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 60.0f, 60.0f)];
+    [_closeButton setImage:[UIImage imageNamed:@"Close Button"] forState:UIControlStateNormal];
+    [_closeButton addTarget:self action:@selector(dismissViewController) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_closeButton];
+}
+
+- (void)dismissViewController
+{
+    [self dismissViewControllerAnimated:YES completion:NULL];
 }
 
 - (UIButton *)postButton
